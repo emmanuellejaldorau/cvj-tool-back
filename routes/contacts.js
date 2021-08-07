ea525284f6dff6cc467f41b9c86cd1b1e83e0383
Emmanuelle Jaldorau <e.jaldorau@gmail.com>
2021-08-07 20:17:14 +0200

    feat(contact): added update route

--- routes/contacts.js
+++ routes/contacts.js
@@ -4,19 +4,35 @@ const Contact = require("../models/contact-model.js");
 
 
 //-----------------------ADD CONTACT -----------------------
-router.post(
-  "/contacts",
-  (req, res, next) => {
-    const { fullName, email, phone } = req.body;
+router.post("/contacts", (req, res, next) => {
+  const { fullName, email, phone } = req.body;
+
+  Contact.create({ fullName, email, phone })
+    .then(contactDoc => {
+      res.json(contactDoc);
+    })
+    .catch(err => next(err));
+});
 
 
-    Contact.create({ fullName, email, phone })
-      .then(contactDoc => {
-        res.json(contactDoc);
-      })
-      .catch(err => next(err));
-  }
-);
+//-----------------------UPDATE CONTACT -----------------------
+router.put('/contacts/:id', (req, res, next) => {
+
+  const { fullName, email, phone } = req.body;
+
+  const updatedContact = new Contact({
+    _id: req.params.id,
+    fullName,
+    email,
+    phone
+  });
+
+  Contact.updateOne({_id: req.params.id}, updatedContact)
+    .then(contactDoc => {
+      res.json(contactDoc);
+    })
+    .catch(err => next(err));
+});
 
 
 //---------------------DELETE CONTACT -----------------------
@@ -32,28 +48,28 @@ router.delete("/contacts/:contactId", (req, res, next) => {
 });
 
 
-//----------------------CONTACT LIST ------------------------
+//----------------------CONTACTS LIST ------------------------
 router.get("/contacts", (req, res, next) => {
   
-    Contact.find()
-      .then(result => {
-        res.json(result);
-      })
-      .catch(err => next(err));
-  });
+  Contact.find()
+    .then(result => {
+      res.json(result);
+    })
+    .catch(err => next(err));
+});
   
   
-  //---------------------CONTACT DETAILED PAGE -----------------------------  
-  router.get("/contacts/:contactId", (req, res, next) => {
+//---------------------CONTACT DETAILED PAGE -----------------------------  
+router.get("/contacts/:contactId", (req, res, next) => {
 
-    const { contactId } = req.params;
+  const { contactId } = req.params;
   
-    Contact.findById(contactId)
-      .then(contactDoc => {
-        res.json(contactDoc);
-      })
-      .catch(err => next(err));
-  });
+  Contact.findById(contactId)
+    .then(contactDoc => {
+      res.json(contactDoc);
+    })
+    .catch(err => next(err));
+});
   
 
 module.exports = router;
